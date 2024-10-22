a4b37660bbd473bacda05341862a9f8e0448d741
Nadirici <nadir1401@gmail.com>
2024-10-22 15:17:10 +0200

    A finir

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hicon");
+
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+
+}
