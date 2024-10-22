02e692e9ce367d0c288573c12fb0deaf6cb19aa8
Nadirici <nadir1401@gmail.com>
2024-10-23 01:46:21 +0200

    Fin de PortFolio

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hicon");
 
+    //attribution de la classe open aux éléments ayant les classes .menu-links et .hicon
     menu.classList.toggle("open")
     icon.classList.toggle("open")
-
 }
